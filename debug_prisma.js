70a96d13d8662a6e599469e1aa67b28845c7f26a
bAht1yer <bahtiyer@outlook.com>
2026-03-06 21:38:33 -0500

    chore: remove dev files and update .gitignore

--- debug_prisma.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-try {
-    const result = execSync('npx prisma generate', { encoding: 'utf8' });
-    fs.writeFileSync('prisma_error.log', "STDOUT:\n" + result);
-} catch (error) {
-    fs.writeFileSync('prisma_error.log', "STDOUT:\n" + (error.stdout || "") + "\nSTDERR:\n" + (error.stderr || ""));
-}
